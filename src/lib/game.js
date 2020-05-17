3e7350d66bbd0dcf9de63e9a13ef8f8553e9e0c1
Kevin Chen <kchen@palantir.com>
2020-05-17 14:15:17 -0700

    split out setup function

--- src/lib/game.js
+++ src/lib/game.js
@@ -1,21 +1,4 @@
-import CHARACTERS from './characters.js';
-
-// Recommended role distribution for different numbers of players
-// http://www.englishsanguosha.com/rules/roles
-const ROLE_DIST_LABELS = ['King', 'Rebel', 'Loyalist', 'Spy'];
-const ROLE_DIST = {
-    2: [1, 1, 0, 0],
-    3: [1, 1, 0, 1],
-    4: [1, 2, 0, 1],
-    5: [1, 2, 1, 1],
-    6: [1, 3, 1, 1],
-    7: [1, 3, 2, 1],
-    8: [1, 4, 2, 1],
-    9: [1, 4, 3, 1],
-    10: [1, 5, 3, 1],
-};
-
-const NUM_CHARACTER_CHOICES = 3;
+import setup from './setup.js';
 
 function findKingPlayer(G) {
     return G.roles.findIndex(role => role.name === 'King');
@@ -91,58 +74,7 @@ const turn = {
 export const SanGuoSha = {
     name: "san-guo-sha",
 
-    setup: ctx => {
-        const { numPlayers, playOrder, random } = ctx;
-
-        const unshuffledRoles = [];
-        ROLE_DIST_LABELS.forEach((role, i) => {
-            for (let j = 0; j < ROLE_DIST[numPlayers][i]; j++) {
-                unshuffledRoles.push({ name: role });
-            }
-        });
-        const roles = random.Shuffle(unshuffledRoles);
-        for (let i = 0; i < roles.length; i++) {
-            roles[i].id = `role-${i}`;
-        }
-
-        const allCharacterChoices = random.Shuffle(CHARACTERS);
-        const characterChoices = Object.fromEntries(playOrder.map((player, i) =>
-            [player, allCharacterChoices.slice(NUM_CHARACTER_CHOICES * i, NUM_CHARACTER_CHOICES * (i + 1))]));
-        const characters = {};
-
-        const unshuffledDeck = [];
-        // TODO use more than one card lol
-        for (let i = 0; i < 10; i++) {
-            unshuffledDeck.push({
-                value: '10',
-                suit: 'CLUB',
-                type: 'Attack',
-            });
-        }
-        for (let i = 0; i < 10; i++) {
-            unshuffledDeck.push({
-                value: '7',
-                suit: 'DIAMOND',
-                type: 'Dodge',
-            });
-        }
-        const deck = random.Shuffle(unshuffledDeck);
-        for (let i = 0; i < deck.length; i++) {
-            deck[i].id = `card-${i}`;
-        }
-
-        const hands = Object.fromEntries(playOrder.map(player => [player, []]));
-        const activeCard = undefined;
-
-        return {
-            roles,
-            characterChoices,
-            characters,
-            deck,
-            hands,
-            activeCard,
-        };
-    },
+    setup,
 
     playerView: (G, ctx, playerID) => {
         const { roles } = G;
