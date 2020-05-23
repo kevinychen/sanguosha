14c88d5ae5410930cfacca993d974be808824085
Kevin Chen <kchen@palantir.com>
2020-05-23 14:06:48 -0700

    audio

--- src/client/lobby.js
+++ src/client/lobby.js
@@ -9,8 +9,22 @@ const GAMES = [{ game: SanGuoSha, board: SanGuoShaBoard }];
 
 export default class SanGuoShaLobby extends React.Component {
 
+    componentDidMount() {
+        // Mobile requires explicit user action to play audio
+        document.querySelector('#lobby-view').addEventListener('click', () => this.props.audio.play());
+    }
+
+    componentWillUnmount() {
+        document.querySelector('#lobby-view').removeEventListener('click');
+    }
+
     render() {
         return <div className='lobby'>
+            <div
+                // TODO remove hack button to turn off sound if too loud
+                style={{ position: 'absolute', width: '100px', height: '100px' }}
+                onClick={() => this.props.audio.volume = 0.2 - this.props.audio.volume}
+            />
             <div className='title'>
                 <img src='./name.png' alt='sanguosha' />
             </div>
--- src/index.js
+++ src/index.js
@@ -4,8 +4,18 @@ import Room from './client/room';
 import Lobby from './client/lobby';
 
 class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.audio = new Audio();
+        this.audio.src = './background.mp3';
+        this.audio.volume = 0.2;
+        this.audio.loop = true;
+        this.audio.play();
+    }
+
     render() {
-        return process.env.REACT_APP_LOCAL ? <Room /> : <Lobby />;
+        return process.env.REACT_APP_LOCAL ? <Room /> : <Lobby audio={this.audio} />;
     }
 }
 
